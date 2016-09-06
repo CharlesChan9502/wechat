aee5812691f51345b109d431e47cc5da9e850f3e
CharlesChan9502 <onlychen9502@qq.com>
2016-09-06 18:13:42 +0800

    code

---
+++ src/com/charles/servlet/WeChatServlet.java
@@ -0,0 +1,84 @@
+package com.charles.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.dom4j.DocumentException;
+
+import com.charles.util.CheckUtil;
+import com.charles.util.MessageUtil;
+
+public class WeChatServlet extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String signature = req.getParameter("signature");
+		String timestamp = req.getParameter("timestamp");
+		String nonce = req.getParameter("nonce");
+		String echostr = req.getParameter("echostr");
+		
+		PrintWriter out = resp.getWriter();
+		if(CheckUtil.checkSignature(signature, timestamp, nonce)){
+			out.print(echostr);
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		resp.setCharacterEncoding("utf-8");
+		PrintWriter out = resp.getWriter();
+		try {
+			Map<String, String> map = MessageUtil.xmlToMap(req);
+			String fromUserName = map.get("FromUserName");
+			String toUserName = map.get("ToUserName");
+			String msgType = map.get("MsgType");
+			String content = map.get("Content");
+			
+			String message= null;
+			if(MessageUtil.MESSAGE_TEXT.equals(msgType)){
+				if("1".equals(content)){
+					message = MessageUtil.initText(toUserName, fromUserName, MessageUtil.firstMenu());
+				}else if("2".equals(content)){
+					message = MessageUtil.initText(toUserName, fromUserName, MessageUtil.secondMenu());
+				}else if("?".equals(content)||"？".equals(content)){
+					message = MessageUtil.initText(toUserName, fromUserName, MessageUtil.menuText());
+					
+				}
+				
+				
+//				TextMessage text = new TextMessage();
+//				text.setFromUserName(toUserName);
+//				text.setToUserName(fromUserName);
+//				text.setMsgType("text");
+//				text.setCreateTime(new Date().getTime()+"");
+//				text.setContent("您发送的消息是："+content);
+//				message = MessageUtil.textMessageToXml(text);
+			}else if(MessageUtil.MESSAGE_EVENT.equals(msgType)){
+				String eventType = map.get("Event");
+				if(MessageUtil.MESSAGE_SUBSCRIBE.equals(eventType)){
+					message = MessageUtil.initText(toUserName, fromUserName, MessageUtil.menuText());
+				}
+			}
+			out.print(message);
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			out.close();
+		}
+		
+	}
+}
