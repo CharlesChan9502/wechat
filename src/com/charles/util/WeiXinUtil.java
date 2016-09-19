1e67a10a2196d26c1fdad2ad902d294570c493cd
Charles Chan <Charles Chan@PC-20160314BXGU>
2016-09-19 17:48:53 +0800

    授权

--- src/com/charles/util/WeiXinUtil.java
+++ src/com/charles/util/WeiXinUtil.java
@@ -1,7 +1,9 @@
 package com.charles.util;
 
 import java.io.IOException;
+import java.util.List;
 
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 import org.apache.http.HttpEntity;
@@ -13,14 +15,13 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
-
-
-
 import com.charles.button.Button;
 import com.charles.button.ClickButton;
 import com.charles.button.Menu;
 import com.charles.button.ViewButton;
 import com.charles.po.AccessToken;
+import com.charles.po.SNSUserInfo;
+import com.charles.po.WeixinOauth2Token;
 
 public class WeiXinUtil {
 	private static final String APPID = "wx6bad3c216a01f1e3";
@@ -131,4 +132,89 @@ public class WeiXinUtil {
 		return result;
 		
 	}
+	/**
+     * 获取网页授权凭证
+     * 
+     * @param appId 公众账号的唯一标识
+     * @param appSecret 公众账号的密钥
+     * @param code
+     * @return WeixinAouth2Token
+	 * @throws IOException 
+	 * @throws ParseException 
+     */
+    @SuppressWarnings("unused")
+	public static WeixinOauth2Token getOauth2AccessToken(String appId, String appSecret, String code) throws ParseException, IOException {
+        WeixinOauth2Token wat = null;
+        // 拼接请求地址
+       /* String requestUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
+        requestUrl = requestUrl.replace("APPID", appId);
+        requestUrl = requestUrl.replace("SECRET", appSecret);
+        requestUrl = requestUrl.replace("CODE", code);*/
+        // 获取网页授权凭证
+        String url = ACCESS_TOKEN_URL.replace("APPID", APPID).replace("APPSECRET", APPSECRET);
+        JSONObject jsonObject = doGetStr(url);
+        if (null != jsonObject) {
+            try {
+                wat = new WeixinOauth2Token();
+                wat.setAccessToken(jsonObject.getString("access_token"));
+                wat.setExpiresIn(jsonObject.getInt("expires_in"));
+                wat.setRefreshToken(jsonObject.getString("refresh_token"));
+                wat.setOpenId(jsonObject.getString("openid"));
+                wat.setScope(jsonObject.getString("scope"));
+            } catch (Exception e) {
+                wat = null;
+				int errorCode = jsonObject.getInt("errcode");
+				String errorMsg = jsonObject.getString("errmsg");
+//                log.error("获取网页授权凭证失败 errcode:{} errmsg:{}", errorCode, errorMsg);
+            }
+        }
+        return wat;
+    }
+    
+    /**
+     * 通过网页授权获取用户信息
+     * 
+     * @param accessToken 网页授权接口调用凭证
+     * @param openId 用户标识
+     * @return SNSUserInfo
+     * @throws IOException 
+     * @throws ParseException 
+     */
+    @SuppressWarnings({ "unchecked", "unused" })
+	public static SNSUserInfo getSNSUserInfo(String accessToken, String openId) throws ParseException, IOException {
+        SNSUserInfo snsUserInfo = null;
+        // 拼接请求地址
+        String requestUrl = "https://api.weixin.qq.com/sns/userinfo?access_token=ACCESS_TOKEN&openid=OPENID";
+        requestUrl = requestUrl.replace("ACCESS_TOKEN", accessToken).replace("OPENID", openId);
+        // 获取网页授权凭证
+        String url = ACCESS_TOKEN_URL.replace("APPID", APPID).replace("APPSECRET", APPSECRET);
+        JSONObject jsonObject = doGetStr(url);
+        if (null != jsonObject) {
+            try {
+                snsUserInfo = new SNSUserInfo();
+                // 用户的标识
+                snsUserInfo.setOpenId(jsonObject.getString("openid"));
+                // 昵称
+                snsUserInfo.setNickname(jsonObject.getString("nickname"));
+                // 性别（1是男性，2是女性，0是未知）
+                snsUserInfo.setSex(jsonObject.getInt("sex"));
+                // 用户所在国家
+                snsUserInfo.setCountry(jsonObject.getString("country"));
+                // 用户所在省份
+                snsUserInfo.setProvince(jsonObject.getString("province"));
+                // 用户所在城市
+                snsUserInfo.setCity(jsonObject.getString("city"));
+                // 用户头像
+                snsUserInfo.setHeadImgUrl(jsonObject.getString("headimgurl"));
+                // 用户特权信息
+                snsUserInfo.setPrivilegeList(JSONArray.toList(jsonObject.getJSONArray("privilege"), List.class));
+            } catch (Exception e) {
+                snsUserInfo = null;
+                int errorCode = jsonObject.getInt("errcode");
+                String errorMsg = jsonObject.getString("errmsg");
+//                log.error("获取用户信息失败 errcode:{} errmsg:{}", errorCode, errorMsg);
+            }
+        }
+        return snsUserInfo;
+    }
 }
